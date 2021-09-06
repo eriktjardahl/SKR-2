0833c21e21a7805eed84ab8db94dd49f52460f17
Erik Tjardahl <erik.tjardahl+github@live.com>
2021-09-06 13:08:15 +0200

    Initial commit

--- Firmware/Marlin-bugfix-2.0.9.1.x/Marlin/Configuration.h
+++ Firmware/Marlin-bugfix-2.0.9.1.x/Marlin/Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(BigTreeTech, SKR-2)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "Erik Tjärdahl, PLEJD" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -147,7 +147,7 @@
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "87680f4a-ba8a-4b77-8dff-d69093fe2ede"
 
 /**
  * Define the number of coordinated linear axes.
